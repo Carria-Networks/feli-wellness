9f211a786339e32c7089f8fafe920336b1bc0fb0
Kelvin <kelvin@tililtech.com>
2025-10-08 14:35:53 +0300

    Removed testimonials

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <Products />
         <DistributorRelationships />
         <About />
-        <Testimonies />
+        {/* <Testimonies /> */}
         <Contacts />
         <Partners />
 
